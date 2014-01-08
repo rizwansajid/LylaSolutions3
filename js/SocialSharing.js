cc2b30678a0ae388b214064d3771d536d55490ec
Arslan <aabhatti76@gmail.com>
2014-01-08 17:37:05 +0500

    sdsdsd

---
+++ js/SocialSharing.js
@@ -0,0 +1,23 @@
+function SocialSharing() {
+}
+
+SocialSharing.prototype.available = function (callback) {
+  cordova.exec(function (avail) {
+    callback(avail ? '1' : '0');
+  }, null, "SocialSharing", "available", []);
+};
+
+SocialSharing.prototype.share = function (message, subject, image, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "SocialSharing", "share", [message, subject, image]);
+};
+
+SocialSharing.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.socialsharing = new SocialSharing();
+  return window.plugins.socialsharing;
+};
+
+cordova.addConstructor(SocialSharing.install);
